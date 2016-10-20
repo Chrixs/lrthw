6ce815d4645983780a09b9ec386d80c059f0b2b3
Chris <chrixs99@hotmail.com>
2016-10-20 19:56:32 +0100

    ex12.rb added

---
+++ ex12.rb
@@ -0,0 +1,16 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+puts "This is the 10% machine! How much would you like to give me?"
+number = gets.chomp.to_f
+puts number / 10
