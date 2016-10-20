23b3196361cf2730d17959c4bae5c8bff0868275
Chris <chrixs99@hotmail.com>
2016-10-20 17:00:25 +0100

    revised ex04.rb

--- ex04.rb
+++ ex04.rb
@@ -1,10 +1,10 @@
 cars = 100
-space_in_a_car = 2.0
+space_in_a_car = 4.0
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
-carpool_capacity = 
+carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available"
@@ -15,5 +15,7 @@ puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
 
 
-# The error message means that there was nothing assigned to the variable "carpool_capacity"
-#
+# Cannot find a reason for his "carpool_capacity" error it says the error occurs on line 14 but "carpool_capacity" is not used on line 14
+# The addition of a floating point number means that the entire equation can now use fractional values
+# The use of it once causes the entire equation to use floating points
+# Though the use of a floating point number here is redundant as you'd rarely be transporting .5 of a person
