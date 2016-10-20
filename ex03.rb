8d10f2f510838a094ac411fc1667f8c38e77afdf
Chris <chrixs99@hotmail.com>
2016-10-20 16:27:09 +0100

    ex03.rb added

---
+++ ex03.rb
@@ -0,0 +1,25 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0 + 30 / 6}"
+puts "Roosters #{100.0 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+# puts the answer of the sum following the PEMDAS order of operations
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+# < in sum will mean true of false will be returned depending on the sum
+puts 3.0 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3.0 + 2}"
+puts "What is 5 - 7? #{5.0 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more?"
+# is 5 greater than -2
+puts "Is it greater? #{5.0 > -2}"
+# is 5 greater than or equal to -2
+puts "Is it greater or equal? #{5.0 >= -2}"
+# is 5 less than or equal to -2
+puts "Is it less or equal? #{5.0 <= -2}"
