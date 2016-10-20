ede95f9a319c9c01d31f9f265b3141ea3d056a61
Chris <chrixs99@hotmail.com>
2016-10-20 18:05:45 +0100

    ex07.rb added

---
+++ ex07.rb
@@ -0,0 +1,21 @@
+puts "Mary had a litle lamb."
+puts "It's fleece was white as #{'snow'}"
+puts "And everywhere that Mary went."
+puts "." * 10
+# "." was printed 10 times
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#puts adds a line, whereas print does not
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
