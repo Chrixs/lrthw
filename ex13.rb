513463a520e5f335ffb40bb5728684f9cf6dc8f6
Chris <chrixs99@hotmail.com>
2016-10-21 13:02:43 +0100

    ex13.rb added

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+first, second, third, fourth, fifth = ARGV
+puts "What's your name?"
+name = $stdin.gets.chomp
+puts "Hi #{name}, todays variables are as follows:"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "Your fourth variable is: #{fourth}"
+puts "Your fifth variable is: #{fifth}"
